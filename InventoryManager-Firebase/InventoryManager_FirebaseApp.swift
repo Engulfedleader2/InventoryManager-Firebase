1fc1841dad20e09b9c880eba087422b87634dd01
Engulfedleader2 <engulfedleader@gmail.com>
2024-08-29 20:55:29 -0400

    Initial Commit

---
+++ InventoryManager-Firebase/InventoryManager_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryManager_FirebaseApp.swift
+//  InventoryManager-Firebase
+//
+//  Created by Israel on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct InventoryManager_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
